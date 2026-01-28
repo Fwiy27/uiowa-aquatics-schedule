35f1290726359e512ad1068915ed1a104be35194
Fwiy27 <landonmigawa27@gmail.com>
2026-01-28 10:00:05 -0600

    fixed logic for getting last day of next month

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.google_calendar import sync_day
 from src.scraper import get_entries
 from dotenv import load_dotenv
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 from zoneinfo import ZoneInfo
 
 import calendar
@@ -16,8 +16,11 @@ TIMEZONE = os.getenv('TIMEZONE', 'America/Chicago')
 
 # TODAY
 from_day = datetime.now(ZoneInfo(TIMEZONE)).date()
+current_month, current_year = from_day.month, from_day.year
+to_month = (current_month) % 12 + 1
+to_year = current_year if to_month != 1 else current_year + 1
 # LAST DAY OF THE NEXT MONTH
-to_day = from_day + timedelta(days=32)
+to_day = date(year=to_year, month=to_month, day=1)
 _, last_day_num = calendar.monthrange(to_day.year, to_day.month)
 to_day = to_day.replace(day=last_day_num)
 
@@ -34,14 +37,14 @@ while current_date <= to_day:
     if notification:
         print(f'\t{notification}')
         notifications.append(f'{current_date.isoformat()}: {notification}')
-        
+
     current_date += timedelta(days=1)
-    
+
 if notifications:
     headers = {
         'Title': 'Updated UIOWA Aquatic Calendar'
     }
-    
+
     try:
         response = requests.post(f'https://ntfy.sh/{NTFY_TOPIC}', data='\n'.join(notifications), headers=headers)
         response.raise_for_status()
